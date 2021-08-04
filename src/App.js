14e2e600d70457aab3748cf12cdd5949f0adf259
Sai Arunesh <vssaiarunesh@gmail.com>
2021-08-04 23:08:38 +0530

    basic interpreter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+const emojiMap = {
+  "💫": "Dizzy",
+  "🐪": "Camel",
+  "🐫": "Two-hump Camel",
+  "☘": "Shamrock",
+  "🥐": "Croissant",
+};
+
+const emojiKeys = Object.keys(emojiMap);
+
+export default function App() {
+  const [inputEmoji, setTranslation] = useState("Please type in");
+
+  function clickEmojiHandler(item) {
+    setTranslation(emojiMap[item]);
+  }
+
+  function inputChangeHandler(event) {
+    console.log(emojiMap[event.target.value]);
+    if (event.target.value !== "") {
+      if (event.target.value in emojiMap) {
+        setTranslation(emojiMap[event.target.value]);
+      } else {
+        setTranslation("Not Available right now");
+      }
+    } else {
+      setTranslation("Please type in");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Interpreter</h1>
+      <input type="text" onChange={inputChangeHandler}></input>
+      <p>{inputEmoji}</p>
+      <ul className="available-emojis">
+        {emojiKeys.map((item, index) => {
+          return (
+            <li
+              key={index}
+              style={{ cursor: "pointer", padding: "20px" }}
+              onClick={() => clickEmojiHandler(item)}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
